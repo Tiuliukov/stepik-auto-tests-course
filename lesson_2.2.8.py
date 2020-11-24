192a532529c6ab163e92ae8fcfa7179fd7538def
Tiuliukov Volodymyr <48677163+Tiuliukov@users.noreply.github.com>
2020-11-24 16:11:46 +0200

    Test for module 2.2.8

---
+++ lesson_2.2.8.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+import os 
+
+try: 
+    link = 'http://suninjuly.github.io/file_input.html'
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    input1 = browser.find_element_by_name("firstname")
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_name("lastname")
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_name("email")
+    input3.send_keys("example@mail.com")
+    
+    current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого файла 
+    file_path = os.path.join(current_dir, 'file.txt')           # добавляем к этому пути имя файла 
+    element = browser.find_element_by_name("file")
+    element.send_keys(file_path)
+    
+    button = browser.find_element_by_css_selector("button")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
+
